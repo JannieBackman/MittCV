9f931570de001cedd6c35435abc62cde3582669e
Jannie87 <jannie.backman@gmail.com>
2021-11-22 13:30:34 +0100

    added sliding sidenav

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function openSideNav() {
-
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("header").style.marginLeft = "250px";
 }
 
 
 function closeSideNav() {
-    
+    document.getElementById("mySideNav").style.width ="0";
+    document.getElementById("header").style.width="0;"
 }
