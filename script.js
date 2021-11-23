e52f79835875cf054463925ab6cb227245ea6af2
Jannie87 <jannie.backman@gmail.com>
2021-11-23 09:55:03 +0100

    added links to github and email

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function openSideNav() {
+
+/** Makes the SideNav slide out when user clicks on hamburger-menu */
+
+function openSideNav(event) {
     document.getElementById("mySideNav").style.width = "250px";
     document.getElementById("header").style.marginLeft = "250px";
 }
 
 
+
+/** Makes the SideNav slide in when user clicks on 'x' -close-button*/
 function closeSideNav() {
     document.getElementById("mySideNav").style.width ="0";
     document.getElementById("header").style.width="0;"
