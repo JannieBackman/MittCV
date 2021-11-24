892e316ed77ac750b9bf0f4804928de78f0c5512
Jannie87 <jannie.backman@gmail.com>
2021-11-24 08:42:50 +0100

    Added new div and new positions to buttons

--- script.js
+++ script.js
@@ -14,10 +14,13 @@ function closeSideNav() {
     document.getElementById("smaller-screens").style.width="0;"
 }
 
-function showHiddenInfo(event){
-    console.log(event.target);
-    let info = event.target.nextSibling;
-   console.log(info);
+
+/**
+ * Function that displays the next sibling p-tag on buttonclick.
+ * @param {*} event 
+ */
+ function showHiddenInfo(event){
+    let info = event.target.nextElementSibling;
     if (info.style.display === "none") {
         info.style.display ="block";
     } else {
